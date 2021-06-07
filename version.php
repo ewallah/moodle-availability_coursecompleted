ece15b1dd91699b46c738aeb2c545eccece25b90
ewallah <info@ewallah.net>
2021-06-07 15:19:16 +0200

    Update version.php

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2021060600;
+$plugin->version   = 2021030701;
 $plugin->requires  = 2020061500;
 $plugin->component = 'availability_coursecompleted';
 $plugin->maturity  = MATURITY_STABLE;
