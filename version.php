bad6804b599275384130ebc9593ae07683475ad6
eWallah <info@eWallah.net>
2021-07-15 12:55:37 +0200

    mariadb 10.5

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2021030701;
+$plugin->version   = 2021060601;
 $plugin->requires  = 2020061500;
 $plugin->component = 'availability_coursecompleted';
 $plugin->maturity  = MATURITY_STABLE;
