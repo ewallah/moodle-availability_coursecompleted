df948f1d6171e1a401c5ce64f83333dd86b69f1e
Renaat Debleu <rdebleu@eWallah.net>
2023-04-23 20:31:58 +0200

    PHP 8.1

--- version.php
+++ version.php
@@ -28,5 +28,6 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->requires = 2020061500;
 $plugin->component = 'availability_coursecompleted';
 $plugin->maturity = MATURITY_STABLE;
+$plugin->supported = [311, 402];
 $plugin->release = 'v3.9.6';
 $plugin->version = 2022040400;
